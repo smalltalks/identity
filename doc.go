290be77d2558fded7e833184f6b41160ef3911de
Tuan Nguyen <nmtuan.dev@gmail.com>
2018-06-25 01:18:52 +0700

    Setup project skeleton

---
+++ doc.go
@@ -0,0 +1,9 @@
+package identity
+
+//go:generate protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. ./pb/identity.proto
+//go:generate protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --swagger_out=logtostderr=true:. ./pb/identity.proto
+//go:generate swagger generate server -f ./pb/identity.swagger.json -A identity
+//go:generate echo Done
+
+// [SKIPPED] Generate reverse HTTP proxy for grpc server
+// go:generate protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. ./pb/identity.proto
